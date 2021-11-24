44c02a2d1e210bcf6b48e012429380838a5f25fa
unknown <riteshpandey1200@gmail.com>
2021-11-24 17:46:26 +0530

    commit

--- app/views.py
+++ app/views.py
@@ -1,17 +1,25 @@
-from types import prepare_class
-
-from rest_framework import permissions
+from rest_framework.views import APIView
 from .models import Blog
 from .serializers import BlogSerializer
-from rest_framework import generics
 from rest_framework.permissions import  IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 
-class BlogList(generics.ListCreateAPIView):  # parent class is generics.ListCreateAPIView it allow to get all data and post the data
+class BlogList(APIView):  # parent class is generics.ListCreateAPIView it allow to get all data and post the data
 
     permission_classes = [IsAuthenticated]  # user must be logged in to get data
 
-    queryset = Blog.objects.all()   # queryset is used for returning all objects of Blog
-    serializer_class = BlogSerializer   # this class is used for serialization
+    def get(self, request):
+        snippets = Blog.objects.all()
+        serializer = BlogSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     
